94f797a1bdd7e65d8ea74f00aa35e869c1ce1708
Jose Chavarria <jose.chavarria@resuelve.mx>
2020-08-06 23:18:00 -0500

    update test

--- test/newproyect3_test.exs
+++ test/newproyect3_test.exs
@@ -2,7 +2,7 @@ defmodule Newproyect3Test do
   use ExUnit.Case
   doctest Newproyect3
 
-  test "greets the world" do
+  test "test date and fibonacci" do
     assert Newproyect3.date ==  Timex.today
     assert Newproyect3.fib(4) == 3
   end
